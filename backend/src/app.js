c67145cf424187f209e2ff4f88fced3fec0f10b0
HappyParmar26 <happyparmar2006@gmail.com>
2026-04-09 18:33:00 +0530

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import connectDB from './config/connectDB.js';
 import authRouter from './routes/auth.routes.js';
@@ -12,6 +13,7 @@ const app = express();
 const PORT = Number(process.env.PORT) || 8000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
