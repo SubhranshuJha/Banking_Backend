485c638fb87847d11a387784a5b8e115440c6ac2
HappyParmar26 <happyparmar2006@gmail.com>
2026-04-11 23:35:27 +0530

    ...

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import React, { createContext } from "react";
+export const DataContext = createContext()
+
+const UserContext = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UserContext
